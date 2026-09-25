ee1d3293595508e85f689e3dc2ec24c542b3b159
koreasambo <mkmk2809@gmail.com>
2026-09-25 15:10:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='photo-judge-unified-v2.3.5';
-const SHELL=['./index.html','./manifest.webmanifest','./GO.png','./HOLD.png','./DROP.png','./icon-192.png','./icon-512.png','./splash.png'];
+const CACHE='photo-judge-unified-v2.3.6';
+const SHELL=['./index.html','./manifest.webmanifest','./GO.png','./HOLD.png','./DROP.png','./icon-192.png','./icon-512.png','./splash.png','./god_eye_bgm.mp3'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
