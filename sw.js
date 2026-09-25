e1bbd1a702d0368c0b5c22098659c3a753f056c2
koreasambo <mkmk2809@gmail.com>
2026-09-25 17:56:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='photo-judge-v240';
+const CACHE='photo-judge-v242';
 const SHELL=['./index.html','./manifest.webmanifest','./GO.png','./HOLD.png','./DROP.png','./icon-192.png','./icon-512.png','./splash.png','./god_eye_bgm.mp3'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
